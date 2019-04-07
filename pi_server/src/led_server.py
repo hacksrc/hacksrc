221801c588fc9e0eeb8577acde2374a7939fad77
meow <meow@mail.com>
2019-04-07 02:43:04 -0400

    led controls added

--- pi_server/src/led_server.py
+++ pi_server/src/led_server.py
@@ -1,4 +1,4 @@
-#!/bin/python
+#!/usr/bin/python
 
 # tcp settings
 import socket
@@ -6,6 +6,15 @@ import sys
 
 # data packing
 from struct import *
+
+# LED Import libs used
+import time
+import random
+import getopt
+from neopixel import *
+
+#Start up random seed
+random.seed()
 
 # program debug specific settings
 mDebug = 1
@@ -21,11 +30,47 @@ mHdrMsgHdrLen = 24
 
 mHdrMsgMagicByte = b'\x00\x00\x00\x07'
 
+# defines for the LEDs
+mEffectCmdFire = 0
+mEffectCmdLevel = 1
+
+# LED strip configuration:
+LED_COUNT      = 50      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
+LED_BRIGHTNESS = 128     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+
+#Predefined Colors and Masks
+OFF = Color(0,0,0)
+WHITE = Color(255,255,255)
+RED = Color(255,0,0)
+GREEN = Color(0,255,0)
+BLUE = Color(0,0,255)
+PURPLE = Color(128,0,128)
+YELLOW = Color(255,255,0)
+ORANGE = Color(255,50,0)
+TURQUOISE = Color(64,224,208)
+RANDOM = Color(random.randint(0,255),random.randint(0,255),random.randint(0,255))
+
+FIRE_COLORS = [RED, ORANGE]
+
+#bitmasks used in scaling RGB values
+REDMASK = 0b111111110000000000000000
+GREENMASK = 0b000000001111111100000000
+BLUEMASK = 0b000000000000000011111111
+
+# Other vars
+LIGHT_ARRAY_LENGTH = 50
+LIGHTSHIFT = 0  #shift the lights down the strand to the other end 
+FLICKERLOOP = 3  #number of loops to flicker
+
 '''
 	parse_msg_payload: parse out the payload data from the buffer
 '''
 def parse_msg_payload(msg_id, data):
-	effect, color, duration = unpack_from('>III', data)
+	effect, color, duration = unpack_from('>IfI', data)
 	if msg_id == mHdrMsgEffectId:
 		print("SERV: effect={}, color={},  duration={}".format(effect, color, duration))
 	return [effect, color, duration]
@@ -94,8 +139,102 @@ def find_msg_hdr(data):
 			print("SERV: magic header not in data")
 	return sub
 
+'''
+	color functions
+'''
+def fireWall(strip):
+  for i in range(0,LIGHT_ARRAY_LENGTH):
+    strip.setPixelColor(i, RED)
+
+  strip.show()
+
+  endTime = time.time() + 10.0
+
+  while (endTime > time.time()):
+    randomLedIndex = random.randint(0,LIGHT_ARRAY_LENGTH)
+
+    #turn off for a random short period of time
+    strip.setPixelColor(randomLedIndex, OFF)
+    strip.show()
+    time.sleep(random.randint(10,50)/1000.0)
+    random_fire_color = random.randint(0,1)
+    strip.setPixelColor(randomLedIndex, FIRE_COLORS[random_fire_color])
+    strip.show()
+
+  #kill all lights
+  for led in range(0, LIGHT_ARRAY_LENGTH):
+    strip.setPixelColor(led+LIGHTSHIFT, OFF)
+  strip.show()
+
+def showPlayerColor(strip, color, duration):
+
+  for i in range(0,LIGHT_ARRAY_LENGTH):
+    strip.setPixelColor(i, color)
+  
+  strip.show()
+
+  time.sleep(duration)
+
+  #kill all lights
+  for led in range(0,LIGHT_ARRAY_LENGTH):
+    strip.setPixelColor(led+LIGHTSHIFT, OFF)
+  strip.show()
+
+def levelUp(strip, color):
+
+  for i in range(0,LIGHT_ARRAY_LENGTH):
+    strip.setPixelColor(i, color)
+
+  strip.show()
+
+  endTime = time.time() + 10.0
+
+  pixelIndex = 0
+  forward = True
+  while (endTime > time.time()):
+
+    if (pixelIndex >= LIGHT_ARRAY_LENGTH-1):
+      forward = False
+    elif (pixelIndex <= 0):
+      forward = True
+
+    #turn off for a random short period of time
+    strip.setPixelColor(pixelIndex, OFF)
+    strip.setPixelColor(pixelIndex+1, OFF)
+    strip.show()
+    time.sleep(0.02)
+    strip.setPixelColor(pixelIndex, color)
+    strip.setPixelColor(pixelIndex+1, color)
+    strip.show()
+
+    if (forward):
+      pixelIndex += 2
+    else:
+      pixelIndex -= 2
+
+
+  #kill all lights
+  for led in range(0, LIGHT_ARRAY_LENGTH):
+    strip.setPixelColor(led+LIGHTSHIFT, OFF)
+  strip.show()
+
+def playerColorLookup(color):
+    return {
+        'red': RED,
+        'green': GREEN,
+        'blue': BLUE,
+        'purple': PURPLE,
+        'yellow': YELLOW,
+        'orange': ORANGE,
+        'turquoise': TURQUOISE
+    }.get(color, WHITE) 
 
 if __name__ == "__main__":
+
+	# Create NeoPixel object with appropriate configuration.
+	strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+	# Intialize the library (must be called once before other functions).
+	strip.begin()
 
 	# server settings
 	mSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -137,4 +276,15 @@ if __name__ == "__main__":
 		msg_id = get_hdr_msg_id(data)
 		data = data[mHdrMsgHdrLen:]
 		effect, color, duration = parse_msg_payload(msg_id, data)
+		
+		# call a command
+		if (effect == mEffectCmdFire):
+			fireWall(strip)
+		elif (effect == mEffectCmdLevel):
+			levelUp(strip, playerColorLookup(color))
+		elif (color != ''):
+			showPlayerColor(strip, playerColorLookup(color), duration)
+		else:
+			print ("SERV: invalid effect!")
+	
 		data = ""
